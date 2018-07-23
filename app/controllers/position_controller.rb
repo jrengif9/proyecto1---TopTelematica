3258d0baa25895e00b8bc4d012beddc2eaf1ce67
jrengif9 <jrengif9@eafit.edu.co>
2018-07-22 22:18:00 -0500

    añadiendo coordenas e inicio de sesion y acoplacion con bootstrap

---
+++ app/controllers/position_controller.rb
@@ -0,0 +1,22 @@
+class PositionController < ApplicationController
+  def getPosition
+  	@positions = (Position.where({user: current_user.id}).reverse)[0,10]
+  end
+
+  def save
+  	respond_to do |format|
+	    latitud = params[:lat]
+	    longitud = params[:lon]
+	    puts "lon: "+longitud.to_str+" lat: "+latitud.to_str
+	    @position = Position.new(user: current_user.id,latitud: latitud, longitud: longitud)
+	    if @position.save
+	          format.html { redirect_to position_getPosition_path }
+	    else
+	        redirect_to position_getPosition_path
+	    end
+	end
+
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+
+  get 'position/getPosition', to: 'position#getPosition'
+  get 'position/save', to: 'position#save'
+
+  root 'position#getPosition'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
